1b64c1fe744b72345bbec90e4a03fb43962d2d67
choiwono <treasure_b@naver.com>
2019-03-12 22:00:57 +0900

    #19.03.12 레이아웃 업데이트!!

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-/*insert into account (id,email,name,nick_name,passwd,reg_date)
+insert into account (id,email,name,nick_name,passwd,reg_date)
 values(1,'treasureb1220@gmail.com','최원오','choiwono1','{bcrypt}$2a$10$o3eFgx5gkg0UPctYnvje7OMSTKhfZJ9H2.X/60UKxH382LeCIcHJ.',NOW());
 
 insert into account (id,email,name,nick_name,passwd,reg_date)
@@ -6,7 +6,6 @@ values(2,'treasureb1330@gmail.com','최원오','choiwono2','{bcrypt}$2a$10$o3eFg
 
 insert into account (id,email,name,nick_name,passwd,reg_date)
 values(3,'admin','최원오','choiwono2','{bcrypt}$2a$10$o3eFgx5gkg0UPctYnvje7OMSTKhfZJ9H2.X/60UKxH382LeCIcHJ.',NOW());
-
 
 insert into roles (id,name,reg_date)
 values(1,'ADMIN',NOW());
@@ -21,4 +20,4 @@ insert into account_roles (account_id,role_id)
 values(2,2);
 
 insert into account_roles (account_id,role_id)
-values(3,1);*/
+values(3,1);
