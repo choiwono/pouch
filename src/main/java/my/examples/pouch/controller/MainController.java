9b513fd43f6be5627ce46bd52b388d39e6ce568e
jaehee0145 <jaeheepark0145@gmail.com>
2019-03-22 18:00:16 +0900

    #19.03.22 롤백 작업

--- src/main/java/my/examples/pouch/controller/MainController.java
+++ src/main/java/my/examples/pouch/controller/MainController.java
@@ -1,15 +1,19 @@
-package my.examples.pouch.controller;
-
 import lombok.RequiredArgsConstructor;
+import my.examples.pouch.domain.Category;
 import my.examples.pouch.service.AccountService;
+import my.examples.pouch.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
     private final AccountService accountService;
+    private final CategoryService categoryService;
 
     @GetMapping(value="/")
     public String index(Model model){
@@ -17,7 +21,12 @@ public class MainController {
     }
 
     @GetMapping(value="/main")
-    public String main(Model model){
+    public String main(Model model,
+                       Principal principal){
+        if(principal != null){
+            List<Category> categories = categoryService.findMyCategoryList(principal.getName());
+            model.addAttribute("categories",categories);
+        }
         return "index";
     }
 }
