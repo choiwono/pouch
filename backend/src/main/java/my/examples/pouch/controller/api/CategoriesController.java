bdc01c939f685d4c41b792a6e7ae6194f964a520
jaehee0145 <jaeheepark0145@gmail.com>
2019-04-17 18:43:15 +0900

    #19.04.17 API 설계

--- backend/src/main/java/my/examples/pouch/controller/api/CategoriesController.java
+++ backend/src/main/java/my/examples/pouch/controller/api/CategoriesController.java
@@ -4,10 +4,8 @@ import lombok.RequiredArgsConstructor;
 import my.examples.pouch.domain.Category;
 import my.examples.pouch.dto.CustomCategory;
 import my.examples.pouch.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -19,17 +17,55 @@ import java.util.List;
 public class CategoriesController {
     private final CategoryService categoryService;
 
-    @GetMapping(value="/{id}")
-    public List<CustomCategory> myCategory(@PathVariable(value="id") Long id
-            ,Principal principal){
+/*기존 코드
+@GetMapping(value="/{id}")
+public List<CustomCategory> myCategory(@PathVariable(value="id") Long id
+,Principal principal){
+List<CustomCategory> list = new ArrayList<>();
+List<Category> categories = categoryService.findMyCategoryList(id);
+for(int i=0; i<categories.size(); i++){
+CustomCategory customCategory = new CustomCategory();
+customCategory.setId(categories.get(i).getId());
+customCategory.setName(categories.get(i).getCategoryName());
+list.add(customCategory);
+}
+return list;
+}
+*/
+
+    //검색해서 카테고리 목록 가져오기
+    @GetMapping(value = "/search")
+    public List<CustomCategory> searchCategories(@RequestParam(name = "searchType") int searchType,
+                                                 @RequestParam(name = "searchStr") String searchStr, Model model) {
         List<CustomCategory> list = new ArrayList<>();
-        List<Category> categories = categoryService.findMyCategoryList(id);
-        for(int i=0; i<categories.size(); i++){
-            CustomCategory customCategory = new CustomCategory();
-            customCategory.setId(categories.get(i).getId());
-            customCategory.setName(categories.get(i).getCategoryName());
-            list.add(customCategory);
-        }
         return list;
+    }
+
+    //특정 유저의 카테고리 목록 가져오기
+    @GetMapping
+    public void getCategoryByAccount(@RequestParam(name = "accountId") Long accountId) {}
+
+    //특정 카테고리 가져오기
+    @GetMapping(value = "/{id}")
+    public void getCategory(@PathVariable(value="id") Long id){}
+
+
+
+    //카테고리 수정하기
+    @PutMapping(value = "/{id}")
+    public void editCategory(@PathVariable(value="id") Long id){
+
+    }
+
+    //카테고리 삭제
+    @DeleteMapping(value = "/{id}")
+    public void deleteCategory(@PathVariable(value = "id") Long id){
+
+    }
+
+    // 다른 유저의 카테고리를 복사해서 내 카테고리로 저장하기
+    @PostMapping
+    public void shareCategory(@RequestParam(name = "id") Long id){
+
     }
 }
