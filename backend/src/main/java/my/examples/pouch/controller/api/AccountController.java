80c01b00b0ef6ac360657a7a55106ce00715e488
jaehee0145 <jaeheepark0145@gmail.com>
2019-04-24 20:39:26 +0900

    #19.04.24 회원가입

--- backend/src/main/java/my/examples/pouch/controller/api/AccountController.java
+++ backend/src/main/java/my/examples/pouch/controller/api/AccountController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
@@ -34,8 +35,8 @@ public class AccountController {
     private final AccountService accountService;
 
     // 계정 토큰 발급
-    @PostMapping(value="/auth")
-    public AccountInfo accountInfo(@AuthenticationPrincipal CustomSecurityUser customSecurityUser){
+    @PostMapping(value = "/auth")
+    public AccountInfo accountInfo(@AuthenticationPrincipal CustomSecurityUser customSecurityUser) {
         AccountInfo accountInfo = new AccountInfo();
         accountInfo.setId(customSecurityUser.getId());
         accountInfo.setEmail(customSecurityUser.getEmail());
@@ -45,61 +46,48 @@ public class AccountController {
 
     //내 정보 가져오기
     @GetMapping(value = "/me")
-    public void getMyAccount(){
+    public void getMyAccount() {
     }
 
     //내 정보 수정하기
     @PutMapping(value = "/me")
-    public void editMyAccount(){}
+    public void editMyAccount() {
+    }
 
     //패스워드 찾기
     @PutMapping(value = "/findpswd")
-    public void findPassword(){}
+    public void findPassword() {
+    }
 
     //이메일 중복 확인하기
     @PostMapping(value = "/emailcheck")
-    public void emailCheck(){}
+    public void emailCheck() {
+    }
 
     //로그인
     @PostMapping(value = "/login")
-    public void login(){}
+    public void login() {
+    }
 
     //로그아웃
     @GetMapping(value = "/logout")
-    public void logout(){}
+    public void logout() {
+    }
 
     //회원가입
     @PostMapping(value = "/join")
-    public void join(@Valid Joinform joinform, BindingResult bindingResult, Model model){
-
-
-    }
-
-    public String joinform(@Valid Joinform joinform,
-                           BindingResult bindingResult, Model model) {
+    public Account join(@Valid Joinform joinform,
+                     BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return "account/joinfalse";
-        }
+            System.out.println("---"); }
 
-        if (!joinform.getPasswd().equals(joinform.getPasswd2())) {
-            model.addAttribute("notEqualPassword", "비밀번호가 서로 일치하지않습니다.");
-            return "account/joinfalse";
-        }
-
-        Account emailCheck = accountService.findAccountByEmail(joinform.getEmail());
-        if (emailCheck != null) {
-            model.addAttribute("duplicateEmail", "중복된 이메일이 존재합니다.");
-            return "account/joinfalse";
-        }
 
         Account account = new Account();
-        account.setEmail(joinform.getEmail());
         account.setName(joinform.getName());
+        account.setEmail(joinform.getEmail());
         account.setNickName(joinform.getNickname());
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         account.setPasswd(passwordEncoder.encode(joinform.getPasswd()));
-        accountService.join(account);
-        return "/account/welcome";
-
+        return accountService.join(account);
     }
 }
