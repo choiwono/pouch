59a562c0ec2fd285312c33c795613024532c63ae
choiwono <treasure_b@naver.com>
2019-05-14 11:34:38 +0900

    #190514 config 변경

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://15.164.26.183:3000/api',
+        target: 'ec2-15-164-26-183.ap-northeast-2.compute.amazonaws.com:3000/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
