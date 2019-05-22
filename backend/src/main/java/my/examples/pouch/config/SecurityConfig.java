e615bae4ed8e9d3c46aa520628e47b8fe6239c46
choiwono <treasure_b@naver.com>
2019-05-22 22:02:44 +0900

    # jsoup url valid 추가

--- backend/src/main/java/my/examples/pouch/config/SecurityConfig.java
+++ backend/src/main/java/my/examples/pouch/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.headers().frameOptions().disable();
         http.httpBasic()
             .and()
                 .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
