968efb5c9acfead5510882a8e2285a43a95e29a0
jaehee0145 <jaeheepark0145@gmail.com>
2019-06-19 18:21:00 +0900

    #19.06.19 메세지 공유 기능

--- backend/src/main/java/my/examples/pouch/service/MessageService.java
+++ backend/src/main/java/my/examples/pouch/service/MessageService.java
@@ -2,8 +2,12 @@ package my.examples.pouch.service;
 
 import my.examples.pouch.domain.Message;
 
+import java.util.List;
+
 public interface MessageService {
     public Message save(Message message);
 
     long countNewMessage(String name);
+
+    List<Message> getMessage(String name);
 }
